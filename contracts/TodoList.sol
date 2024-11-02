2d27a73b561c82b763ef920cb4ec62457fb63023
SarthakRana21 <sarthak.rana0321@gmail.com>
2024-11-02 20:09:30 +0530

    updated app.js

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,7 +1,26 @@
 pragma solidity ^0.5.0;
+import "./safemath.sol";
 
 contract TodoList {
+
+    using SafeMath for uint;
+
 	uint public taskCount = 0;
     
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("this is a web3 todo dapp");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount = taskCount.add(1);
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 	
 }
